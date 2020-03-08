e53e154c8c9e8d59b2ddb95e467e0b7cbbce6429
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2020-03-08 15:19:11 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/madstap/igviz3.clj
+++ src/madstap/igviz3.clj
@@ -68,20 +68,20 @@
         key->node (medley/index-by :igviz.node/key nodes)
         _ (do (println "key->node: ") (prn key->node))
         edges
-        (map (fn make-edge [[src dest :as edge]]
-               (println "make-edge, edge: " (pr-str edge))
-               (let [refs                    (edge-refs key->node edge)
-                     _ (do (println "refs: ") (prn refs))
-                     [src-id dest-id :as id] (mapv normalize-key edge)]
-                 (println "id: " (pr-str id))
-                 #:igviz.edge{:src             src
-                              :src-id          src-id
-                              :dest            dest
-                              :dest-id         dest-id
-                              :edge            edge
-                              :id              id
-                              :refs            refs}))
-             edges*)]
+        (mapv (fn make-edge [[src dest :as edge]]
+                (println "make-edge, edge: " (pr-str edge))
+                (let [refs                    (edge-refs key->node edge)
+                      _ (do (println "refs: ") (prn refs))
+                      [src-id dest-id :as id] (mapv normalize-key edge)]
+                  (println "id: " (pr-str id))
+                  #:igviz.edge{:src             src
+                               :src-id          src-id
+                               :dest            dest
+                               :dest-id         dest-id
+                               :edge            edge
+                               :id              id
+                               :refs            refs}))
+              edges*)]
     (println "Edges: " (prn-str edges))
     #:igviz{:nodes nodes, :edges (set edges)}))
 
